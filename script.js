02152214cb68533bfa53124099243f90c0972a93
Andrew <andrew80ka@gmail.com>
2019-09-11 20:30:20 +0300

    add js-file (start project)

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 'use strict';
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
-let arr = ['apple', 'orange', 'cherry'];
+let money = +prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-console.log(arr[1]);
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+
+let expenseItem1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let expenseItemValue1 = +prompt('Во сколько обойдется?', '');
+let expenseItem2 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let expenseItemValue2 = +prompt('Во сколько обойдется?', '');
+
+appData.expenses.expenseItem1 = expenseItemValue1;
+appData.expenses.expenseItem2 = expenseItemValue2;
+
+let daysInMonth = 30;
+let budgetOneDay = Math.floor(appData.budget / daysInMonth);
+
+alert('Бюджет на один день: ' + budgetOneDay + '.');
